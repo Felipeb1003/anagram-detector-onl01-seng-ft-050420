2ff344b85a701d16591c3182e63dee259db755b1
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-16 03:05:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,6 @@ class Anagram
   
   def match(array)
     
-    array.select { |words| words.split("").sort == @name.split("").sort}
+   array.select {|words| words.split("").sort == @name.split("").sort}
      end
 end
